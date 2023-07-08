1e1133e24a7ac482084d8a00b5c8a5b97df32a63
CurieLuo <CurieLuo@qq.com>
2023-07-08 16:06:32 +0800

    book2-img13

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -114,6 +114,6 @@ impl NoiseTexture {
 
 impl Texture for NoiseTexture {
     fn value(&self, _u: f64, _v: f64, p: Point3) -> Color {
-        Color::new(1., 1., 1.) * self.noise.turb7(p * self.scale)
+        Color::new(1., 1., 1.) * 0.5 * (1. + (self.scale * p.z + 10. * self.noise.turb7(p)).sin())
     }
 }
